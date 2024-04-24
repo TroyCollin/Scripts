62390b441db797cb396b7b5671c9805045e2498f
Troy Collin <tcollin@nattech.net>
2024-04-24 16:34:33 -0400

    setup starting script

---
+++ Lets_Encrypt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+# Next Dimension ASCII art.
+echo "Next Dimension Inc"
+
+echo "    _   _   _   _     _   _   _   _   _   _   _   _   _     _   _   _  "
+echo "   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ "
+echo "  ( N | e | x | t ) ( D | i | m | e | n | s | i | o | n ) ( I | n | c )"
+echo "   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ "
+echo
+echo " script by Troy Collin"
+echo
+echo " This script will renew Let's Encrypt and restart Nginx"
+echo ""
+echo ""
+echo "v.1.0"
+
+
+

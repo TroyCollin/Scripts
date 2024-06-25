f4437a7b29d02d92a372ae36bed351a531dae86a
Troy Collin <75144078+TroyCollin@users.noreply.github.com>
2024-06-25 17:31:21 -0400

    Update UPdate_Lets_Encrypt.sh

--- UPdate_Lets_Encrypt.sh
+++ UPdate_Lets_Encrypt.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+clear
+# Next Dimension ASCII art.
+echo "Next Dimension Inc"
+
+echo "    _   _   _   _     _   _   _   _   _   _   _   _   _     _   _   _  "
+echo "   / \ / \ / \ / \   / \ / \ / \ / \ / \ / \ / \ / \ / \   / \ / \ / \ "
+echo "  ( N | e | x | t ) ( D | i | m | e | n | s | i | o | n ) ( I | n | c )"
+echo "   \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ "
+echo
+echo " script by Troy Collin"
+echo
+echo " This script will renew Let's Encrypt and restart Nginx"
+echo ""
+echo ""
+echo "v.1.0"
+
+
+
+#### start variables ###
+log_file="./certexpiry.txt"
+
+### variables end   ###
+
+
 # Function to check if a certificate is due for renewal
 check_renewal() {
     # Change to your actual certificate path
-    cert_path="/etc/letsencrypt/live/example.com/fullchain.pem"
+    cert_path="/etc/letsencrypt/live/systech.nattech.net/fullchain.pem"
 
     # Check if certificate exists
     if [ -f "$cert_path" ]; then
@@ -31,13 +55,24 @@ check_renewal() {
 
 # Function to renew the certificate
 renew_certificate() {
-    echo "Renewing Let's Encrypt certificate..."
+   echo "Renewing Let's Encrypt certificate..."
+    
     certbot renew
+    STATUS="$(systemctl is-active nginx.service)"
+if [ "${STATUS}" = "active" ]; then
+    echo "restarting Nginx"
+    systemctl restart nginx 
+else 
+    echo " Service not running.... so exiting "  
+    exit 1  
+fi
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
-        echo "Certificate renewed successfully."
+        echo "Certificate renewed successfully." 
+         echo $host",Cert renewed successfully "$(date -d '+%Y-%m-%d') >> $log_file
     else
         echo "Failed to renew certificate. Exit code: $exit_code"
+        echo $host",Failed to renew Cert "$(date -d '+%Y-%m-%d') >> $log_file
         exit $exit_code
     fi
 }
@@ -47,4 +82,7 @@ if check_renewal; then
     renew_certificate
 else
     echo "Certificate renewal is not yet needed."
+    echo $(date -u) $host" Cert renewal not needed" >> $log_file
+    
+    
 fi
